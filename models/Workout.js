dabfd02d2915edffa1da0d6961315f32dbcf057f
Sharon Kim <sharonkim1709@gmail.com>
2020-07-25 13:07:02 -0400

    can successfully post when using pizza schema layout

--- models/Workout.js
+++ models/Workout.js
@@ -4,17 +4,30 @@ const Schema = mongoose.Schema;
 
 // defining workout schema
 const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
+  // day: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+  // exercises:[{
+  //   type: {
+  //     type: Schema.Types.ObjectId,
+  //     // use populate() in query
+  //     ref: "Exercise", //must match model 
+  //   }
+  // }]
+  name:{
+    type:String,
+     trim:true,
+     required:"Workout name is required",
   },
-  exercises:[{
-    type: {
-      type: Schema.Types.ObjectId,
-      // use populate() in query
-      ref: "Exercise", //must match model 
-    }
-  }]
+  price:{
+    type:String, 
+    trim:true, 
+    required:"Pizza price sis required"
+  },
+  size:{
+    type:Number,
+  }
 });
 
 // exporting model on mongoose instance passing in collection and schema
