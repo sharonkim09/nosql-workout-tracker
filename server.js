6419c40dc7666df20a921bbda9cd3442860cccef
Sharon Kim <sharonkim1709@gmail.com>
2020-07-25 16:19:10 -0400

    moved code and added more comments

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-// require express
+// Dependancies
 const express = require("express");
-// require mongoose
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const db = require("./models");
+
 // instance of express
 const app = express();
-
-const logger = require("morgan");
-
+// defining port
 const PORT = process.env.PORT || 3000;
-
-// const db = require("./models/Workout")
-const db = require("./models");
+// use morgan logger
 app.use(logger("dev"));
+
 // for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
+// boilerplate for mongoose connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
 // Requiring routes
 require("./routes/html-routes.js")(app);
 app.use(require("./routes/api-routes"))
 
-
+// listener for port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
