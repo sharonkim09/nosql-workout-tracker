7f2efe0b613a3459d7fad0aae3b443c176ed9d9e
Sharon Kim <sharonkim1709@gmail.com>
2020-07-25 19:28:46 -0400

    can successfully add totalDuration by virtual property

--- models/Workout.js
+++ models/Workout.js
@@ -40,8 +40,20 @@ const WorkoutSchema = new Schema({
       required: "Distance is required. Enter distance value",
     },
   }]
+},
+// not stored in db but to read/compute the property specified
+{
+  toJSON:{
+    virtuals:true
+  }
 });
-
+// computes the totalDuration via a virtual
+WorkoutSchema.virtual("totalDuration").get(function() {
+  // reduce returns only one value 
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 // exporting model on mongoose instance passing in collection and schema
 const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
